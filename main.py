e26458eeaea1abe7c2f364f05cac6e63308ccaa4
IniyanKanmani <kiniyan002@gmail.com>
2024-11-04 21:40:06 +0000

    added player mechanics

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import pygame
+
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
-    # print("Starting asteroids!")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    Player.containers = (updatable, drawable)
 
     while True:
         for event in pygame.event.get():
@@ -15,7 +23,18 @@ def main():
                 return
 
         screen.fill("#000000")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
+
+        for obj in updatable:
+            obj.update()
+
+        for obj in drawable:
+            obj.draw()
+
+        rest = clock.tick(60)
+        dt = rest / 1000
 
 
 if __name__ == "__main__":
